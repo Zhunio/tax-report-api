013683d983fb929a6fc37bbb4712a5181b56ac20
Richard Zhuno <967968+Zhunio@users.noreply.github.com>
2023-01-07 17:34:57 -0500

    1: Add tax report controller/service for creating tax report

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TaxReportController } from './tax-report/tax-report.controller';
+import { TaxReportService } from './tax-report/tax-report.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [AppController, TaxReportController],
+  providers: [AppService, PrismaService, TaxReportService],
 })
 export class AppModule {}
