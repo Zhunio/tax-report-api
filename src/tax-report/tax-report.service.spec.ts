2a5a1eff2c138acc41c83a73a5950c3a2e91d399
Richard Zhuno <967968+Zhunio@users.noreply.github.com>
2023-02-19 01:45:15 -0500

    2: Add deleteTaxReport unit test

--- src/tax-report/tax-report.service.spec.ts
+++ src/tax-report/tax-report.service.spec.ts
@@ -6,6 +6,7 @@ import { TaxReportService } from './tax-report.service';
 class MockPrismaService {
   taxReport = {
     create: jest.fn(),
+    delete: jest.fn(),
   };
 }
 
@@ -31,16 +32,46 @@ describe('TaxReportService', () => {
     expect(taxReportService).toBeDefined();
   });
 
-  it('should create tax report', async () => {
-    const createTaxReportDto: Prisma.TaxReportCreateInput = {
-      fiscalQuarter: 1,
-      fiscalYear: today.getFullYear(),
-    };
+  describe('createTaxReport()', () => {
+    it('should create tax report', async () => {
+      const createTaxReportDto: Prisma.TaxReportCreateInput = {
+        fiscalQuarter: 1,
+        fiscalYear: today.getFullYear(),
+      };
 
-    await taxReportService.createTaxReport(createTaxReportDto);
+      await taxReportService.createTaxReport(createTaxReportDto);
 
-    expect(prismaService.taxReport.create).toHaveBeenCalledWith({
-      data: createTaxReportDto,
+      expect(prismaService.taxReport.create).toHaveBeenCalledWith({
+        data: createTaxReportDto,
+      });
+    });
+  });
+
+  describe('deleteTaxReport()', () => {
+    it('should delete tax report', async () => {
+      const deleteTaxReportId = 1;
+
+      await taxReportService.deleteTaxReport(deleteTaxReportId);
+
+      expect(prismaService.taxReport.delete).toHaveBeenCalledWith({
+        where: {
+          id: deleteTaxReportId,
+        },
+      });
+    });
+
+    it('should throw an error when trying to delete report that does not exist', async () => {
+      jest.spyOn(prismaService.taxReport, 'delete').mockImplementation(() => {
+        throw new Error();
+      });
+
+      const deleteTaxReportId = 1;
+
+      try {
+        await taxReportService.deleteTaxReport(deleteTaxReportId);
+      } catch (error) {
+        expect(error).toEqual(expect.any(Error));
+      }
     });
   });
 });
