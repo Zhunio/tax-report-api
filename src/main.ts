a7024da384f0b4a3421ed289dbc2570e8bd2d099
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2023-09-06 23:43:12 -0400

    read port from environment

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
+  const port = process.env.PORT || 3000;
+
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
