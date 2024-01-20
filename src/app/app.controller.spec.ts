76198e256edb55da6a69bd6d2bfbb9374f2b64ff
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2024-01-20 12:37:30 -0500

    add version to app controller

--- src/app/app.controller.spec.ts
+++ src/app/app.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { APP_VERSION } from './version';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,15 +8,14 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    appController = app.get(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('getVersion()', () => {
+    it('should return "Welcome to Budget API vx.x.x"', () => {
+      expect(appController.getVersion()).toBe(`Welcome to Budget API v${APP_VERSION}`);
     });
   });
 });
