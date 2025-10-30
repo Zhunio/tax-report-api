5b706b743e08242bd81fae6f1ae22e054e413848
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2025-10-30 19:56:31 -0400

    terraform: remove tfstate

--- infrastructure/modules/backend-setup/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "backend_bucket_name" {
-  description = "The name of the S3 bucket used for the terraform backend"
-  value       = local.backend_bucket_name
-}
--- infrastructure/modules/media-bucket/main.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_s3_bucket" "tax_report_media_bucket" {
-  bucket = local.tax_report_media_bucket_name
-}
