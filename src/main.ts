5121cc3fad19342e89eb61983b6d66bfbbe84ab9
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2026-05-21 23:39:25 -0400

    update prisma to 6.19.2

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const prismaService = app.get(PrismaService);
   const port = configService.get('PORT') || 3000;
 
   app.enableCors();
-  await prismaService.enableShutdownHooks(app);
+  app.enableShutdownHooks();
   await app.listen(port);
 }
 
